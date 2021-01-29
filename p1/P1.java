2410ac6e45a4eaa2d367bc53665490e1c2df3194
yusen <liuys1997@gmail.com>
2021-01-28 23:19:37 -0600

    SymTable.java finished

---
+++ p1/P1.java
@@ -0,0 +1,11 @@
+/**
+ * TestP1
+ * 
+ * This is a class with a purpose to test all of the Sym and SymTable operations 
+ * and all situations under which exceptions are thrown. 
+ * 
+ * 
+ * 
+ * 
+ * Also think about testing both “boundary” and “non-boundary” cases.
+ */
--- p1/SymTable.java
+++ p1/SymTable.java
@@ -1,10 +1,12 @@
 import java.util.*;
 
+
 public class SymTable{
     private List<HashMap<String, Sym>> hashMapList;
     
     SymTable(){
         this.hashMapList = new LinkedList<HashMap<String, Sym>>();
+        this.hashMapList.add(0, new HashMap<String, Sym>());
     }
     
     public void addDecl(String name, Sym sym) throws DuplicateSymException, 
@@ -20,11 +22,58 @@ public class SymTable{
             throw new DuplicateSymException();
         }
         else{
-            this.hashMapList.addF
+            this.hashMapList.get(0).put(name, sym);
         }
 
     }
 
+    public void addScope(){
+        this.hashMapList.add(0, new HashMap<String, Sym>());
+    }
+
+    public Sym lookupLocal(String name) throws EmptySymTableException{
+        if(this.hashMapList.isEmpty()){
+            throw new EmptySymTableException();
+        }
+        else if(this.hashMapList.get(0).containsKey(name)){
+            return this.hashMapList.get(0).get(name);
+        }
+        else{
+            return null;
+        }
+    }
+    
+    public Sym lookupGlobal(String name) throws EmptySymTableException{
+        if(this.hashMapList.isEmpty()){
+            throw new EmptySymTableException();
+        }
+
+        for(HashMap<String, Sym> singleMap : this.hashMapList){
+            if(singleMap.containsKey(name)){
+                return singleMap.get(name);
+            }
+        }
+
+        return null;
+    }
+
+    public void removeScope() throws EmptySymTableException{
+        if(this.hashMapList.isEmpty()){
+            throw new EmptySymTableException();
+        }
+        else{
+            this.hashMapList.remove(0);
+        }
+    }
+
+    public void print(){
+        System.out.println("\nSym Table\n");
+
+        for(HashMap<String, Sym> singleMap : this.hashMapList){
+            System.out.println(singleMap.toString());
+        }
+        System.out.println();
+    }
 
 }
 
