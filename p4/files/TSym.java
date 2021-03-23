ec0f01d100b30de0a8f56422824ecdb850d562de
yusen <liuys1997@gmail.com>
2021-03-23 17:27:02 -0500

    skeleton code for p4 added

---
+++ p4/files/TSym.java
@@ -0,0 +1,15 @@
+public class TSym {
+    private String type;
+    
+    public TSym(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String toString() {
+        return type;
+    }
+}
