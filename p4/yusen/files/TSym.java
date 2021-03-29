b09573d821b652fbdf19ca17fcb7be7028f0805b
yusen <liuys1997@gmail.com>
2021-03-29 16:07:32 -0500

    debug finished

---
+++ p4/yusen/files/TSym.java
@@ -0,0 +1,96 @@
+import java.util.*;
+
+
+public class TSym {
+    private String type;
+    private LinkedList<String> formalTypeList; // FnSym
+    private String structName; // struct decl
+    private SymTable fields; // struct decl
+
+    private boolean isVar; //VarSym
+    private boolean isFunc; // FnSym
+    private boolean isStruct; // struct decl
+    private boolean isStructVar; // struct node_t a;
+    
+    public TSym(String type) {
+        this.type = type;
+        this.formalTypeList = null;
+        this.structName = null;
+        this.fields = null;
+        this.isVar = false;
+        this.isFunc = false;
+        this.isStruct = false;
+        this.isStructVar = false;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String toString() {
+        return type;
+    }
+
+    public LinkedList<String> getTypeList(){
+        return formalTypeList;
+    }
+
+    public void setTypeList(LinkedList<String> l){
+        this.formalTypeList = l;
+    }
+
+    public String getStructName(){
+        return structName;
+    }
+
+    public void setStructName(String s){
+        this.structName = s;
+    }
+
+    public SymTable getSymTable(){
+        return this.fields;
+    }
+
+    public void setSymTable(SymTable t){
+        this.fields = t;
+    }
+
+    public void setVar(){
+        this.isVar = true;
+    }
+
+    public void setFunc(){
+        this.isFunc = true;
+    }
+
+    public void setStruct(){
+        this.isStruct = true;
+    }
+
+    public void setStructVar(){
+        this.isStructVar = true;
+    }
+
+    public boolean isStruct(){
+        if(this.isStruct){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isStructVar(){
+        if(this.isStructVar){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean hasEmptySymTable(){
+        if(this.fields == null){
+            return true;
+        }
+        return false;
+    }
+    
+}
+
